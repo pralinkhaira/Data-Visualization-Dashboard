fa7fc1c156b511e757b4d76584e59a5cece442f7
BitH0xer <132118661+PralinKhaira@users.noreply.github.com>
2023-06-04 15:12:33 +0530

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import pandas as pd
 
 # Read data from a CSV file or any other data source
-data = pd.read_csv('your_data.csv')
+data = pd.read_csv('data.csv')
 
 # Create visualizations using Plotly Express
 bar_chart = px.bar(data, x='Category', y='Count', title='Data by Category')
